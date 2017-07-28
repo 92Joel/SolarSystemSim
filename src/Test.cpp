a0c1ae028b5ebaf5103b109e331ed05b6a65a53f
joel <joel.eggleston@gmail.com>
2017-07-28 23:28:24 +0100

    Changed value of G

--- src/Test.cpp
+++ src/Test.cpp
@@ -2,7 +2,7 @@
 #include <math.h>
 #include <vector>
 
-double G = 6.67e-20;        // Universal gravitational constant km^3 kg^-1 days^-2
+double G = 6.68e-20;        // Universal gravitational constant km^3 kg^-1 days^-2
 double m_sun = 1.98e30;        // Mass of sun in kg
 double m_earth = 5.972e24;  // Mass of earth in kg 
 double earth_sun = 149.6e6; // Earth sun distance in km 
